84fa4478353a82ad87b3f9b3987c0c5c6fce3555
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2019-05-18 00:18:32 +0530

    setup URL type identifier

--- cli.js
+++ cli.js
@@ -5,6 +5,8 @@ const ora = require('ora');
 const meow = require('meow');
 const getLink = require('./util/get-link');
 const getTrack = require('./util/get-track');
+const urlParser = require('./util/url-parser');
+
 const download = require('./lib/downloader');
 
 const cli = meow(
@@ -39,10 +41,32 @@ if (!input[0]) {
   const spinner = ora(`Searching…`).start();
   try {
     for (const link of input) {
-      
+      const urlType = await urlParser(link);
+      switch(urlType) {
+        case 'song': {
+          console.log('Song');
+          break;
+        }
+        case 'playlist': {
+          console.log('Playlist');
+          break;
+        }
+        case 'album': {
+          console.log('Album');
+          break;
+        }
+        case 'artist': {
+          console.log('Artist');
+          break;
+        }
+        default: {
+          throw new Error('Invalid URL type');
+        }
+      }
     }
   } catch (error) {
     spinner.fail(`Something went wrong!`);
+    console.log(error);
     process.exit(1);
   }
 })();
---
+++ util/url-parser.js
@@ -0,0 +1,20 @@
+'use strict';
+const parser = async (inputUrl) => {
+    if(inputUrl.includes('/track/')) {
+        return 'song';
+    }
+    else if (inputUrl.includes('/playlist/')) {
+        return 'playlist';
+    }
+    else if (inputUrl.includes('/album/')) {
+        return 'album';
+    }
+    else if(inputUrl.includes('/artist/')) {
+        return 'artist';
+    }
+    else {
+        return new Error(`Invalid spotify URL`);
+    }
+}
+
+module.exports = parser;
