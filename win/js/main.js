506903e008ba0d9ef57a1a32877e5748eb094d77
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2019-07-08 23:19:40 +0530

    app: support downloading tracks [2/5]

--- win/js/main.js
+++ win/js/main.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const exec = require('child_process').exec;
-// const downloadIt = require('./download-it');
 
 const URL = document.getElementById('urlBox');
 const choose_file_btn = document.getElementById('choose-file');
@@ -9,28 +8,31 @@ const output_path = document.getElementById('output-path');
 const dload = document.getElementById('daunlood');
 //set default os music path
 output_path.value = getDefaultMusicFolder(process.platform);
+var counter = 0;
 
 dload.addEventListener('click', function() {
     if(URL.value) {
-        var counter = 0, progress;
+        console.log("Clicked!");
+        var progress;
         var spotifydl = exec(`spotifydl ${URL.value} -o ${output_path.value} -s false`);
         var execCounts = 0;
         spotifydl.stdout.on('data',async (data) => {
             execCounts++;
             if(data.includes('Song:')) {
                 // console.log(data.split(' '));
-                appendCard(counter);
+                await appendCard(counter);
                 var songName = await getSong(data.split(' '));
                 var outputName = await getOutput(output_path.value, songName);
-                setSong(songName, outputName, counter);
+                await setSong(songName, outputName, counter);
             }
             if(data.includes('Downloading')) {
-                if(execCounts < 2) {
+                if(execCounts < 5) {
                     var total_size = data.split(' ')[3];
+                    console.log(total_size);
+                    await setTotal(total_size, counter);
                 }
-                console.log(total_size);
                 progress = await getProgress(data.split(' '));
-                console.log(progress);
+                await updateProgress(progress, counter);
             }
             if(data.includes("complete")) {
                 console.log("Thai gyu download");
@@ -45,7 +47,28 @@ choose_file_btn.addEventListener('click', function() {
     select_path.click();
 });
 
+async function updateProgress(progress, counter) {
+    const curr_card = document.getElementsByClassName('out-of');
+    curr_card[counter].innerHTML = progress;
+
+    const totul = document.getElementsByClassName('totul')[counter].innerHTML;
+    var percentage = await getPercentage(parseFloat(progress, 10), parseFloat(totul, 10));
+    // console.log(percentage);
+    document.getElementsByClassName('progress-bar')[counter].style.width = percentage + '%';
+}
+
+async function getPercentage(current, total) {
+    return (current/total) * 100;
+}
+
+async function setTotal(total, counter) {
+    console.log("counter:",counter);
+    const curr_card = document.getElementsByClassName('totul');
+    curr_card[counter].innerHTML = total;
+}
+
 async function setSong(nam, out, counter) {
+    console.log("Counter:",counter);
     const curr_card = document.getElementById(counter);
     curr_card.children[0].children[0].children[1].innerHTML = nam;
     curr_card.children[0].children[2].innerHTML = out;
@@ -60,6 +83,7 @@ async function appendCard(id) {
                             <h6 class="card-title d-inline" style="margin-top: 0px; padding-left: 8px"></h6>
                         </div>
                         <small class="card-subtitle d-block mt-2 text-muted">
+                            Downloaded <span class="out-of"></span> of <span class="totul"></span>
                             <div class="progress" style="height: 20px;">
                                 <div class="progress-bar" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
                             </div>
