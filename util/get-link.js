0907b54fe29e1b8dab20cd930b914a538e21502f
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2020-05-06 22:22:13 +0530

    util: getLink: change documentation

--- util/get-link.js
+++ util/get-link.js
@@ -8,9 +8,9 @@ const search = promisify(youtubeSearch);
  * This function searches youtube for given songname and returns the link of topmost result
  *
  * @param {String} songName name of song
- * @returns {String} youtube link of music video
+ * @returns {Promise<String>} youtube link of music video
  */
-const getLink = async songName => {
+const getLink = async (songName) => {
   try {
     const result = await search(songName);
     
