05ed3b21466b1ba933000c0b7bd38a0ea9dc4b4d
jack <jack.carpenter@agworld.com>
2021-06-08 22:08:44 +0800

    added all category types
    * added logic to handle overlapping sections

--- util/constants.js
+++ util/constants.js
@@ -1,4 +1,12 @@
 module.exports = {
+  AUTH: {
+    SCOPES: {
+      USERS_SAVED_PLAYLISTS: 'playlist-read-private',
+      USERS_TOP_TRACKS: 'user-top-read',
+      USERS_SAVED_TRACKS_ALBUMS: 'user-library-read',
+    },
+    STATE: 'some-random-state',
+  },
   INPUT_TYPES: {
     SONG: 'song',
     PLAYLIST: 'playlist',
@@ -9,18 +17,23 @@ module.exports = {
     SAVED_TRACKS: 'savedTracks',
     SAVED_PLAYLISTS: 'savedPlaylists',
   },
-  AUTH: {
-    SCOPES: {
-      USERS_SAVED_PLAYLISTS: 'playlist-read-private',
-      USERS_TOP_TRACKS: 'user-top-read',
-      USERS_SAVED_TRACKS_ALBUMS: 'user-library-read',
-    },
-    STATE: 'some-random-state',
+  FFMPEG: {
+    ASET: 'asetpts=PTS-STARTPTS'
   },
   MAX_LIMIT_DEFAULT: 50,
   SERVER: {
     PORT: 7654,
     HOST: 'localhost',
     CALLBACK_URI: '/callback',
+  },
+  SPONSOR_BLOCK: {
+    CATEGORIES: {
+      SPONSOR: 'sponsor',
+      INTRO: 'intro',
+      OUTRO: 'outro',
+      INTERACTION: 'interaction',
+      SELF_PROMO: 'selfpromo',
+      MUSIC_OFF_TOPIC: 'music_offtopic',
+    },
   },
 };
