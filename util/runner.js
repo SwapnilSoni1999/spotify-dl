e3cf6ec5c3cc7c4150aaebb872b2d7e20158ddda
Yonah Forst <yonaforst@hotmail.com>
2021-07-19 17:14:51 +0200

    declare helper functions within runner.run()
    This so they have access to arguments past in by the caller (and don't have to rely on global variables from cliInputs)

--- util/runner.js
+++ util/runner.js
@@ -14,77 +14,82 @@ const SpotifyExtractor = require('./get-songdata');
 const { inputs, extraSearch, output, outputOnly } = cliInputs();
 
 module.exports = {
-  trackOutputDir: track => {
-    const outputDir = path.normalize(output);
-    return outputOnly ? outputDir : path.join(
-      outputDir,
-      filter.cleanOutputPath(track.artist_name),
-      filter.cleanOutputPath(track.album_name),
-    );
-  },
-  downloadLoop: async function (list) {
-    const spinner = getSpinner();
-    const tracks = list.tracks;
-    const remainingTracks = tracks.filter(track => !track.cached);
-    const tracksCount = tracks.length;
-    const remainingTracksCount = remainingTracks.length;
-    const currentCount = tracksCount - remainingTracksCount + 1;
-    if (!remainingTracksCount) {
-      spinner.succeed(`All songs already downloaded for ${list.name}!\n`);
-    } else {
-      const nextTrack = remainingTracks[0];
-      const trackDir = this.trackOutputDir(nextTrack);
-      const trackId = nextTrack.id;
-      const trackName = nextTrack.name;
-      const albumName = nextTrack.album_name;
-      const artistName = nextTrack.artist_name;
-      spinner.info(
-        [
-          `${currentCount}/${tracksCount}`,
-          `Artist: ${artistName}`,
-          `Album: ${albumName}`,
-          `Song: ${trackName}`,
-        ].join('\n'),
+  run: async function () {
+
+    const trackOutputDir = track => {
+      const outputDir = path.normalize(output);
+      return outputOnly ? outputDir : path.join(
+        outputDir,
+        filter.cleanOutputPath(track.artist_name),
+        filter.cleanOutputPath(track.album_name),
       );
-      const ytLinks = nextTrack.URL ? [nextTrack.URL] : await getLinks(
-        {
-          trackName,
-          albumName,
-          artistName,
-          extraSearch,
-        },
-      );
-      if (ytLinks.length) {
-        const output = path.resolve(
-          trackDir,
-          `${filter.cleanOutputPath(trackName)}.mp3`,
+    };
+
+    const downloadLoop = async function (list) {
+      const spinner = getSpinner();
+      const tracks = list.tracks;
+      const remainingTracks = tracks.filter(track => !track.cached);
+      const tracksCount = tracks.length;
+      const remainingTracksCount = remainingTracks.length;
+      const currentCount = tracksCount - remainingTracksCount + 1;
+      if (!remainingTracksCount) {
+        spinner.succeed(`All songs already downloaded for ${list.name}!\n`);
+      } else {
+        const nextTrack = remainingTracks[0];
+        const trackDir = trackOutputDir(nextTrack);
+        const trackId = nextTrack.id;
+        const trackName = nextTrack.name;
+        const albumName = nextTrack.album_name;
+        const artistName = nextTrack.artist_name;
+        spinner.info(
+          [
+            `${currentCount}/${tracksCount}`,
+            `Artist: ${artistName}`,
+            `Album: ${albumName}`,
+            `Song: ${trackName}`,
+          ].join('\n'),
         );
-        await downloader(ytLinks, output);
-        await mergeMetadata(output, nextTrack);
-        cache.writeId(trackDir, trackId);
-      }
-      // we mark as cached to continue
-      list.tracks = list.tracks.map(track => {
-        if (track.id == trackId) {
-          track.cached = true;
+        const ytLinks = nextTrack.URL ? [nextTrack.URL] : await getLinks(
+          {
+            trackName,
+            albumName,
+            artistName,
+            extraSearch,
+          },
+        );
+        if (ytLinks.length) {
+          const output = path.resolve(
+            trackDir,
+            `${filter.cleanOutputPath(trackName)}.mp3`,
+          );
+          await downloader(ytLinks, output);
+          await mergeMetadata(output, nextTrack);
+          cache.writeId(trackDir, trackId);
         }
+        // we mark as cached to continue
+        list.tracks = list.tracks.map(track => {
+          if (track.id == trackId) {
+            track.cached = true;
+          }
+          return track;
+        });
+        await downloadLoop(list);
+      }
+    };
+
+    const downloadList = async function (list) {
+      const spinner = getSpinner();
+      list.name = list.name.replace('/', '-');
+      spinner.info(`Downloading: ${list.name}`);
+      spinner.info(`Total Songs: ${list.tracks.length}`);
+      list.tracks = list.tracks.map(track => {
+        track.cached = cache.findId(track.id, trackOutputDir(track));
         return track;
       });
-      await this.downloadLoop(list);
-    }
-  },
-  downloadList: async function (list) {
-    const spinner = getSpinner();
-    list.name = list.name.replace('/', '-');
-    spinner.info(`Downloading: ${list.name}`);
-    spinner.info(`Total Songs: ${list.tracks.length}`);
-    list.tracks = list.tracks.map(track => {
-      track.cached = cache.findId(track.id, this.trackOutputDir(track));
-      return track;
-    });
-    await this.downloadLoop(list);
-  },
-  run: async function () {
+      await downloadLoop(list);
+    };
+
+
     const spotifyExtractor = new SpotifyExtractor();
     const spinner = getSpinner();
     for (const input of inputs) {
@@ -92,7 +97,7 @@ module.exports = {
       switch (input.type) {
         case INPUT_TYPES.SONG: {
           const track = await spotifyExtractor.getTrack(URL);
-          await this.downloadList({
+          await downloadList({
             tracks: [
               track,
             ],
@@ -101,13 +106,13 @@ module.exports = {
           break;
         }
         case INPUT_TYPES.PLAYLIST: {
-          await this.downloadList(
+          await downloadList(
             await spotifyExtractor.getPlaylist(URL),
           );
           break;
         }
         case INPUT_TYPES.ALBUM: {
-          await this.downloadList(
+          await downloadList(
             await spotifyExtractor.getAlbum(URL),
           );
           break;
@@ -116,7 +121,7 @@ module.exports = {
           const artistAlbumInfos = await spotifyExtractor.getArtistAlbums(URL);
           for (let x = 0; x < artistAlbumInfos.length; x++) {
             spinner.info(`Starting album ${x + 1}/${artistAlbumInfos.length}`);
-            await this.downloadList(artistAlbumInfos[x]);
+            await downloadList(artistAlbumInfos[x]);
           }
           break;
         }
@@ -124,7 +129,7 @@ module.exports = {
           const savedAlbumsInfo = await spotifyExtractor.getSavedAlbums();
           for (let x = 0; x < savedAlbumsInfo.length; x++) {
             spinner.info(`Starting album ${x + 1}/${savedAlbumsInfo.length}`);
-            await this.downloadList(savedAlbumsInfo[x]);
+            await downloadList(savedAlbumsInfo[x]);
           }
           break;
         }
@@ -134,16 +139,16 @@ module.exports = {
             spinner.info(
               `Starting playlist ${x + 1}/${savedPlaylistsInfo.length}`,
             );
-            await this.downloadList(savedPlaylistsInfo[x]);
+            await downloadList(savedPlaylistsInfo[x]);
           }
           break;
         }
         case INPUT_TYPES.SAVED_TRACKS: {
-          await this.downloadList(await spotifyExtractor.getSavedTracks());
+          await downloadList(await spotifyExtractor.getSavedTracks());
           break;
         }
         case INPUT_TYPES.YOUTUBE: {
-          await this.downloadList({
+          await downloadList({
             tracks: [
               {
                 name: URL,
