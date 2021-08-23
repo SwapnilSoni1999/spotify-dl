b56758bd1a7da055f48cb6b8ef9e80645e712eae
jack <jack.carpenter@agworld.com>
2021-08-23 14:26:44 +0800

    few more reworks
    * used meow to get current version instead of pkg
    * removed the recursive calls to downloadLoop and change downloadLists to just forloop for readability

--- util/version-checker.js
+++ util/version-checker.js
@@ -1,5 +1,5 @@
-const axios = require('axios').default;
-const pkg = require('../package.json');
+import axios from 'axios';
+import meow from 'meow';
 
 const checkVersion = async () => {
   const res =
@@ -7,6 +7,8 @@ const checkVersion = async () => {
       'https://api.github.com/repos/SwapnilSoni1999/spotify-dl/tags',
     );
   const latestVersion = res.data[0].name;
+  const pkg = meow('', { importMeta: import.meta }).pkg
+
   if (pkg.version !== latestVersion) {
     console.log([
       '\n========Update Available========',
