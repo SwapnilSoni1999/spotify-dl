a67d60268b04476ca6fe7be34c629268a3a3741f
Jack <5182053+phyzical@users.noreply.github.com>
2024-08-03 16:52:53 +0800

    Update version-checker.js

--- util/version-checker.js
+++ util/version-checker.js
@@ -6,22 +6,22 @@ const checkVersion = async () => {
     const res = await axios.default(
       'https://api.github.com/repos/SwapnilSoni1999/spotify-dl/tags'
     );
+    const latestVersion = res.data[0].name;
+    const pkg = meow('', { importMeta: import.meta }).pkg;
+  
+    if (pkg.version !== latestVersion) {
+      console.log(
+        [
+          '\n========Update Available========',
+          'Use npm install -g spotify-dl',
+          'to update the package.',
+          '================================\n',
+        ].join('\n')
+      );
+    }
   } catch (_e) {
      console.log("Could not check current version, have checked too many times skipping");
      return;
-  }
-  const latestVersion = res.data[0].name;
-  const pkg = meow('', { importMeta: import.meta }).pkg;
-
-  if (pkg.version !== latestVersion) {
-    console.log(
-      [
-        '\n========Update Available========',
-        'Use npm install -g spotify-dl',
-        'to update the package.',
-        '================================\n',
-      ].join('\n')
-    );
   }
 };
 
